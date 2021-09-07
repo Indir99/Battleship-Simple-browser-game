0035c2b7f5b87490bf428798f7f9bec2c069e72f
indir <indir.karabegovic@hotmail.com>
2021-09-07 16:43:00 +0200

    model and view object are finished

--- battleship.js
+++ battleship.js
@@ -65,11 +65,81 @@ var model = {
             }
         }
         return true;
-    }
+    },
+    generateShipLocations: function() {
+        var locations;
+        for (var i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+    
 };
 
-//A=0, B=1, C=2, D=3, E=4, F=5, G=6
+var controller = {
+    guesses: 0,
+    processGuess: function(guess){
+        var location = parseGuess(guess);
+        if(location){
+            this.guesses++;
+            var hit = model.fire(location);
+            if(hit && model.shipsSunk === model.numShips){
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
 
+    },
+    
+};
+
+function parseGuess(guess){
+    var aplhabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if(guess===null||guess.length!==2){
+        alert("Oops, please enter a letter and a number on the board");
+    } else{
+        firstChar = guess.charAt(0);
+        var row = aplhabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+        if(isNaN(row) || isNaN(column)){
+            alert("Oops, that isn't on the board");
+        } else if( row<0 || row>=model.boardSize || column<0 || column>=model.boardSize){
+            alert("Oops, that's of the board");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+function init() {
+    var fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+   guessInput.onkeypress= handleKeyPress;
+}
+
+function handleFireButton () {
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+    controller.processGuess(guess);
+    guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+   var fireButton = document.getElementById("fireButton");
+   if (e.keyCode === 13) {
+      fireButton.click();
+      return false;
+   }
+}
+
+window.onload = init;
+
+
+//A=0, B=1, C=2, D=3, E=4, F=5, G=6
 //view.displayHit("06");
 //view.displayMiss("13");
 //view.displayHit("24");
@@ -85,13 +155,30 @@ var model = {
 //view.displayHit("16");
 //view.displayMiss("01");
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10");
+//model.fire("53");
+//model.fire("06");
+//model.fire("16");
+//model.fire("26");
+//model.fire("34");
+//model.fire("24");
+//model.fire("44");
+//model.fire("12");
+//model.fire("11");
+//model.fire("10");
+
+//console.log(parseGuess("A0"));
+//console.log(parseGuess("B6"));
+//console.log(parseGuess("G3"));
+//console.log(parseGuess("H0"));
+//console.log(parseGuess("A7"));
+
+//controller.processGuess("A0");
+//controller.processGuess("A6");
+//controller.processGuess("B6");
+//controller.processGuess("C6");
+//controller.processGuess("C4");
+//controller.processGuess("D4");
+//controller.processGuess("E4");
+//controller.processGuess("B0");
+//controller.processGuess("B1");
+//controller.processGuess("B2");
